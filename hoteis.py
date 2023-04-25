4dc081fbcc97238e6bd30daf3a6c6bc577e78ee1
leon junior <leondodejr@gmail.com>
2023-04-25 18:48:20 -0300

    Add csv opener

---
+++ hoteis.py
@@ -0,0 +1,18 @@
+import os
+hotel = []
+nota = []
+valor = []
+
+
+def carregar_dados_do_arquivo():
+  if not os.path.isfile("hoteis.csv"):
+    return
+  
+  with open("hoteis.csv", "r") as arq:
+    linhas = arq.readlines()
+
+    for linha in linhas:
+      partes = linha.split(";")
+      hotel.append(partes[0])
+      nota.append(int(partes[1]))
+      valor.append(float(partes[2][0:-1]))    
